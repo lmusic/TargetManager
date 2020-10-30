979cb8f6649fd4e5a941ec6ab4ce2a796607f163
d.denisko <d.denisko@wellyes.ru>
2020-10-30 17:17:49 +0300

    Implement template for client

---
+++ Client/target-manager/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'target-manager';
+  firstName = 'Sometimes there'
+  lastName = 'will be first and last name authenticated\'s user'
+}
